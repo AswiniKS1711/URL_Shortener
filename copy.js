b02ddefb1c4f09932db95a34c3cd3fed20589f9d
ASWINI KUMAR SAHOO <86346739+AswiniKS1711@users.noreply.github.com>
2023-09-24 22:26:29 +0530

    Initial commit

---
+++ copy.js
@@ -0,0 +1,8 @@
+let newURL=document.getElementById("shorturl");
+let copyBtn=document.getElementById("copy");
+
+copyBtn.onclick = ()=> {
+    newURL.select();
+
+    window.navigator.clipboard.writeText(newURL.value);
+}
